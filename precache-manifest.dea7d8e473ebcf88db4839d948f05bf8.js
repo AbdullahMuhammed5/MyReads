b71ce5bcae9eb354d4923a611e777a938538f61e
abdullah muhammed <abdo56668@gmail.com>
2018-11-15 10:08:56 +0200

    Updates

---
+++ precache-manifest.dea7d8e473ebcf88db4839d948f05bf8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "727662128af4693dee1b",
+    "url": "/MyReads/static/js/main.72766212.chunk.js"
+  },
+  {
+    "revision": "2ee3f7c41127949fa254",
+    "url": "/MyReads/static/js/1.2ee3f7c4.chunk.js"
+  },
+  {
+    "revision": "727662128af4693dee1b",
+    "url": "/MyReads/static/css/main.e44ee0d7.chunk.css"
+  },
+  {
+    "revision": "8744842ec382e92d1003f32852a921ae",
+    "url": "/MyReads/index.html"
+  }
+];
